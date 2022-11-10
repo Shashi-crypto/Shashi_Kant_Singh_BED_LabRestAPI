2616b4f16328fbf817d248b39323bc3a63afca14
Shashi-crypto <68938931+Shashi-crypto@users.noreply.github.com>
2022-11-10 23:27:45 +0530

    Update data.sql file

--- student/src/main/resources/Data.sql
+++ student/src/main/resources/Data.sql
@@ -1,8 +1,8 @@
 -- Insert data into Student Table
-INSERT INTO STUDENT VALUES (1, 'Suresh Reddy','B.Tech','India');
-INSERT INTO STUDENT VALUES (2, 'Murali Mohan','B.Arch','Canada');
-INSERT INTO STUDENT VALUES (3, 'Daniel Denson','B.Tech','New Zealand');
-INSERT INTO STUDENT VALUES (4, 'Tanya Gupta','B.Com','USA');
+INSERT INTO STUDENT (student_id, name, department, country)  VALUES (1, 'Suresh Reddy','B.Tech','India');
+INSERT INTO STUDENT (student_id, name, department, country)  VALUES (2, 'Murali Mohan','B.Arch','Canada');
+INSERT INTO STUDENT (student_id, name, department, country)  VALUES (3, 'Daniel Denson','B.Tech','New Zealand');
+INSERT INTO STUDENT (student_id, name, department, country)  VALUES (4, 'Tanya Gupta','B.Com','USA');
 
 
 
